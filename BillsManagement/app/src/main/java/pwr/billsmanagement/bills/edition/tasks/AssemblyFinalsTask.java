22d5c09d63e52756fbbbdedc7c3a18ad276e5acd
rafal_szyszka_sg <rafal@szyszka.it>
2017-05-28 21:59:54 +0200

    fixed, adding empty product

--- BillsManagement/app/src/main/java/pwr/billsmanagement/bills/edition/tasks/AssemblyFinalsTask.java
+++ BillsManagement/app/src/main/java/pwr/billsmanagement/bills/edition/tasks/AssemblyFinalsTask.java
@@ -2,7 +2,9 @@ package pwr.billsmanagement.bills.edition.tasks;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
+import android.widget.Toast;
 
 import com.orhanobut.logger.Logger;
 
@@ -13,6 +15,7 @@ import pwr.billsmanagement.bills.BillEntity;
 import pwr.billsmanagement.bills.edition.products.FinalProduct;
 import pwr.billsmanagement.bills.edition.products.FinalProductAssembler;
 import pwr.billsmanagement.bills.edition.view.FinalProductView;
+import pwr.billsmanagement.billslist.BillsList;
 
 /**
  * Created by Squier on 22.05.2017.
@@ -39,7 +42,14 @@ public class AssemblyFinalsTask extends AsyncTask<ArrayList<FinalProductView>, V
     @Override
     protected Void doInBackground(ArrayList<FinalProductView>... params) {
         for (FinalProductView view : params[0]) {
-            finalProducts.add(assembler.assembly(view));
+            FinalProduct finalProduct = assembler.assembly(view);
+            if(
+                    !finalProduct.getName().replaceAll(" ", "").isEmpty()  &&
+                    !finalProduct.getTotalPrice().replaceAll(" ", "").isEmpty()
+            ) {
+                finalProducts.add(finalProduct);
+                Logger.i("ADDED PRODUCT: " + finalProduct.toString());
+            }
         }
         return null;
     }
@@ -52,6 +62,9 @@ public class AssemblyFinalsTask extends AsyncTask<ArrayList<FinalProductView>, V
 
         BillEntity bill = new BillEntity(shopName, finalProducts);
         Logger.i("Read bill: " + bill.toString());
+        Intent billsIntent = new Intent(context, BillsList.class);
+        Toast.makeText(context, context.getText(R.string.successfully_added_bill), Toast.LENGTH_LONG).show();
+        context.startActivity(billsIntent);
     }
 
     public void setContext(Context context) {
