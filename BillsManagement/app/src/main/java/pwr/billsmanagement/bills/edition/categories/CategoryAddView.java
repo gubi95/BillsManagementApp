de2d2cbf1f081cb668483c58047a2ea2a9cd9a35
rafal_szyszka_sg <rafal@szyszka.it>
2017-05-28 21:00:31 +0200

    small improvements

--- BillsManagement/app/src/main/java/pwr/billsmanagement/bills/edition/categories/CategoryAddView.java
+++ BillsManagement/app/src/main/java/pwr/billsmanagement/bills/edition/categories/CategoryAddView.java
@@ -6,9 +6,17 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.PopupWindow;
+import android.widget.Spinner;
+
+import java.util.Properties;
 
 import pwr.billsmanagement.R;
+import pwr.billsmanagement.readers.PropertiesReader;
+
+import static pwr.billsmanagement.bills.edition.categories.Keys.CATEGORIES_PROPERTIES;
 
 /**
  * Created by Squier on 25.05.2017.
@@ -17,6 +25,7 @@ import pwr.billsmanagement.R;
 public class CategoryAddView {
 
     private PopupWindow addCategoryView;
+    private View popupView;
     private WindowManager windowManager;
     private Context context;
 
@@ -28,18 +37,30 @@ public class CategoryAddView {
     public PopupWindow getCategoryAddView() {
 
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View popupView = inflater.inflate(R.layout.bill_edit_category, null);
+        popupView = inflater.inflate(R.layout.bill_edit_category, null);
 
         addCategoryView = new PopupWindow(
-                popupView, getMetrics().widthPixels - 250, getMetrics().heightPixels/2
+                popupView, getMetrics().widthPixels - 250, getMetrics().heightPixels/3
         );
+
+        setButtonsListeners();
 
         return addCategoryView;
     }
 
     public void showPopup(View parent) {
         addCategoryView.setElevation(5.0f);
+        addCategoryView.setFocusable(true);
         addCategoryView.showAtLocation(parent, Gravity.CENTER, 0, 0);
+    }
+
+    public void setButtonsListeners() {
+        Button addCategory = (Button) popupView.findViewById(R.id.addCategory);
+        Button cancelAdd = (Button) popupView.findViewById(R.id.cancelAddCategory);
+
+        addCategory.setOnClickListener(v -> addCategoryView.dismiss());
+        cancelAdd.setOnClickListener(v -> addCategoryView.dismiss());
+
     }
 
     private DisplayMetrics getMetrics() {
---
+++ BillsManagement/app/src/main/java/pwr/billsmanagement/bills/edition/categories/Keys.java
@@ -0,0 +1,12 @@
+package pwr.billsmanagement.bills.edition.categories;
+
+/**
+ * Created by Squier on 28.05.2017.
+ */
+
+public class Keys {
+
+    public static final String CATEGORIES_PROPERTIES = "properties/categories.properties";
+    public static final String CATEGORIES_VALUES = "categories_values";
+
+}
